aa66a3b46e047d94f8145e1833fc57fb7eafbcaf
Sumit <sumitk.85@gmail.com>
2014-02-28 06:42:27 -0800

    Mongo connect class to connect to MongoDB

---
+++ mongo.py
@@ -0,0 +1,21 @@
+"""
+This is the python module for connecting and executing queries to MongoDB through python
+"""
+import pymongo
+from pymongo import MongoClient
+
+class MongoConnect(object):
+    """
+    This class has functions to connect to MongoDB
+    """
+
+    SERVER = "localhost"
+    PORT   = "27017"
+
+    def __init__(self, **kwargs):
+        self.client = MongoClient()
+        self.client = MongoClient(self.SERVER, self.PORT)
+
+    def conectDatabase(self, databaseName):
+        self.database = self.client.databasename
+        return self.database
